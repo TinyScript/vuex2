4f3bf90eff6d6977a2640afdb04b6933c4538fed
Tiny <453616448@qq.com>
2016-11-25 14:50:09 +0800

    update

--- src/router.js
+++ src/router.js
@@ -3,11 +3,7 @@ import firstComponent from './components/firstComponent.vue';
 import secondCompoent from './components/secondComponent.vue';
 
 const routers = [
-<<<<<<< HEAD
 	{ path: '/*', redirect: { name : 'Home' } },
-=======
-	{ path: '/*', redirect: { name : '/' } },
->>>>>>> origin/master
 	{ path: '/', name: 'Home', component: Home },
 	{ path: '/first/:id', component: firstComponent },
 	{ path: '/second', component: secondCompoent }
